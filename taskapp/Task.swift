8ed5ceeb6056cab7dabb675013828b3d1816c8d5
MkYmst <frog377@gmail.com>
2020-11-09 16:40:22 +0900

    Realmでタスクを保存する

---
+++ taskapp/Task.swift
@@ -0,0 +1,28 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by MikaYamashita on 2020/11/08.
+//  Copyright © 2020 mika.yamashita. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object{
+    //管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    //タイトル
+    @objc dynamic var title = ""
+    
+    //内容
+    @objc dynamic var contents = ""
+    
+    //日時
+    @objc dynamic var date = Date()
+    
+    //idをプライマリーキーとして設定
+    override static func primaryKey() -> String?{
+        return "id"
+    }
+}
